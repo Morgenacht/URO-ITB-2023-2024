13c6ae5250354c571f2de175fe9ab5ae22d04797
Morgenacht <151203064+Morgenacht@users.noreply.github.com>
2023-11-26 14:14:31 +0700

    Update POST DAY MODUL 2 13022004 Hans Morgan Lie URO.cpp

--- MODUL 2/POST DAY MODUL 2 13022004 Hans Morgan Lie URO.cpp
+++ MODUL 2/POST DAY MODUL 2 13022004 Hans Morgan Lie URO.cpp
@@ -4,8 +4,6 @@
 #include <cmath>
 using namespace std;
 
-const double PI = 3.14159265358979323846;
-
 void lokasi(int sumx, int sumy) {
     cout << "(" << sumx << "," << sumy << ")" << endl;
 }
@@ -13,15 +11,6 @@ void lokasi(int sumx, int sumy) {
 void gerak(int &sumx, int &sumy, int nilai[2]) {
     sumx += nilai[0];
     sumy += nilai[1];
-}
-
-void gerak_2(int &sumx, int &sumy, double v, double t, double theta) {
-    // Convert theta to radians
-    double theta_rad = theta * (PI / 180.0);
-
-    // Calculate the new position based on velocity, time, and angle
-    sumx += static_cast<int>(v * t * cos(theta_rad));
-    sumy += static_cast<int>(v * t * sin(theta_rad));
 }
 
 void save(int sumx, int sumy) {
@@ -51,7 +40,7 @@ int main() {
     int sumy = 0;
     string command {};
 
-    cout << "Func: GO, GO2, CHECK, SAVE, LOAD, SKIP)" << endl;
+    cout << "Func: GO, CHECK, SAVE, LOAD, SKIP)" << endl;
 
     while (!(command == "SKIP")) {
         cin >> command;
@@ -63,11 +52,6 @@ int main() {
                 cin >> nilai[i];
             }
             gerak(sumx, sumy, nilai);
-        } else if (command == "GO2") {
-            double v, t, theta;
-            cout << "Masukkan kecepatan (v), waktu (t), dan sudut (theta) dalam derajat: ";
-            cin >> v >> t >> theta;
-            gerak_2(sumx, sumy, v, t, theta);
         } else if (command == "CHECK") {
             cout << "POSISI DRONE SEKARANG DI ";
             lokasi(sumx, sumy);
@@ -76,7 +60,7 @@ int main() {
         } else if (command == "LOAD") {
             load(sumx, sumy);
         } else if (!(command == "SKIP")) {
-            cout << "Tulis GO untuk lanjut, GO2 untuk simulasi gerak, CHECK untuk cek lokasi, SAVE untuk simpan koordinat, LOAD untuk muat koordinat, & SKIP untuk stop" << '\n';
+            cout << "Tulis GO untuk lanjut, CHECK untuk cek lokasi, SAVE untuk simpan koordinat, LOAD untuk muat koordinat, & SKIP untuk stop" << '\n';
         }
     }
     cout << "DRONE SELESAI DENGAN POSISI TERAKHIR DI (" << sumx << "," << sumy << ")" << endl;
